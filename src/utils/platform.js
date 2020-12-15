95b3c5e94b4344c06b3d46fc30bf273c980cfa17
comWang <tooreason@outlook.com>
2020-12-15 11:02:42 +0800

    feat: update sendMessageToApp

--- src/utils/platform.js
+++ src/utils/platform.js
@@ -13,8 +13,11 @@ const isIOSPrepared =
   window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.AppModel
 const isAndroidPrepared = window.android && window.android.postMessage
 
-
-export const sendMessageToApp = config => {
+export const sendMessageToApp = (code, data) => {
+  const config = {
+    funcNO: code.toString(),
+    object: data,
+  }
   if (isIOS && isIOSPrepared) {
     window.webkit.messageHandlers.AppModel.postMessage(config)
   } else if (isAndroid && isAndroidPrepared) {
@@ -22,27 +25,6 @@ export const sendMessageToApp = config => {
   }
 }
 export const isAppEnvironment = isIOSPrepared || isAndroidPrepared
-export const shareToApp = (options = {}) => {
-  const config = {
-    funcNO: '50003',
-    object: {
-      title: titleDOM.innerText,
-      ...options,
-    },
-  }
-  sendMessageToApp(config)
-}
-
-export const backToApp = (options = {}) => {
-  const config = {
-    funcNO: '50005',
-    object: {
-      ...options,
-    },
-  }
-  sendMessageToApp(config)
-}
-
 /**
  * 50003 分享APP
  * 50004 设置导航栏
